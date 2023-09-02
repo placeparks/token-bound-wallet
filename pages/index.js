21ddcb39389e88195b7e89ac587480506c7d9f64
placeparks <86215469+placeparks@users.noreply.github.com>
2023-09-03 00:25:45 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,27 @@ import {
   Web3Button,
   useAddress,
   useContract,
-  useOwnedNFTs
+  useOwnedNFTs,
+  useTransferNFT
 } from "@thirdweb-dev/react"
 import { nftDropAddress } from "../const/constants"
 import Container from "../components/Container/Container"
 import toast from "react-hot-toast"
 import toastStyle from "../util/toastConfig"
+import { useState } from "react"
 
 /**
  * The home page of the application.
  */
 const Home = () => {
+  const [transferTo, setTransferTo] = useState("");
+  const [tokenId, setTokenId] = useState("");
+
+
   const address = useAddress()
   const { contract: nftDropContract } = useContract(nftDropAddress, "nft-drop")
   const { data: nfts, isLoading } = useOwnedNFTs(nftDropContract, address)
-
+  const { mutate: transfer, isLoading: transferLoading } = useTransferNFT(nftDropContract);
   return (
     <div className={styles.mainPage}>
     <Container maxWidth="lg" >
@@ -58,7 +64,62 @@ const Home = () => {
             >
               Claim NFT
             </Web3Button>
+  
           </div>
+          <div className={styles.row_center} style={{ width: "50vh" }}>
+      <input
+           className={styles.input}
+           style={{ borderRadius: "5px 0 0 5px" }}
+        type="text"
+        placeholder="Transfer To Address"
+        value={transferTo}
+        onChange={e => setTransferTo(e.target.value)}
+      />
+      <input
+           className={styles.input}
+           style={{ borderRadius: "5px 0 0 5px" }}
+        type="text"
+        placeholder="Token ID"
+        value={tokenId}
+        onChange={e => setTokenId(e.target.value)}
+      />
+      <button
+      className={styles.button}
+      style={{
+        marginTop: 0,
+        width: "130px",
+        borderRadius: "0 5px 5px 0"
+      }}
+        onClick={() =>
+          transfer(
+            {
+              to: transferTo,
+              tokenId: tokenId,
+              amount: 1
+            },
+            {
+              onSuccess: () => {
+                toast("NFT Transferred!", {
+                  icon: "✅",
+                  style: toastStyle,
+                  position: "bottom-center"
+                });
+              },
+              onError: e => {
+                console.log(e);
+                toast(`NFT Transfer Failed! Reason: ${e.message}`, {
+                  icon: "❌",
+                  style: toastStyle,
+                  position: "bottom-center"
+                });
+              }
+            }
+          )
+        }
+      >
+        Transfer
+      </button>
+    </div>
         </div>
       ) : (
         <div className={styles.container}>
